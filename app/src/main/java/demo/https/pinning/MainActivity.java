43ee5deba6cfa5ba8ccbf5378f76cc168f85e231
MM0n5Ter <2711730270@qq.com>
2022-10-28 17:26:25 +0800

    SimpleHttp

--- app/src/main/java/demo/https/pinning/MainActivity.java
+++ app/src/main/java/demo/https/pinning/MainActivity.java
@@ -3,8 +3,20 @@ package demo.https.pinning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSimple,btnOKhttp;
+    private EditText requestAddress;
+    private String defaultURL = "www.baidu.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +24,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnSimple = (Button) findViewById(R.id.simple_https_request);
+        btnOKhttp = (Button) findViewById(R.id.okhttp_https_request);
+        requestAddress = (EditText) findViewById(R.id.RequestAddress);
+
+        btnSimple.setOnClickListener(new btnSimpleClick());
+        btnOKhttp.setOnClickListener(new btnOKhttpClick());
+    }
+
+    class btnSimpleClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String newURL = requestAddress.getText().toString().trim();
+            if(newURL.length()==0) {newURL = defaultURL;}
+            try{
+                URL url = new URL("https"+newURL);
+
+            } catch (Throwable e) {
+                Toast ero = Toast.makeText(getApplicationContext(), "出戳啦\n" + e.toString(),Toast.LENGTH_SHORT);
+                ero.show();
+            }
+        }
+    }
+
+    class btnOKhttpClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            return;
+        }
+    }
 }
