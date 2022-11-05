58ef35a2db797a5ad03202b4e4be004c2bb75e14
MMonster <mm0n57er@gmail.com>
2022-11-05 20:44:20 +0800

    "More Beautiful"

--- app/src/main/java/demo/https/pinning/MainActivity.java
+++ app/src/main/java/demo/https/pinning/MainActivity.java
@@ -1,32 +1,20 @@
 package demo.https.pinning;
 
 import demo.https.contact.*;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 import android.os.Bundle;
 import android.os.Looper;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.IOException;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnSimple,btnOKhttp;
     private EditText requestAddress;
-    private String defaultURL = "www.baidu.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,59 +25,54 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        btnSimple = (Button) findViewById(R.id.simple_https_request);
-        btnOKhttp = (Button) findViewById(R.id.okhttp_https_request);
+        Button btnSimple = (Button) findViewById(R.id.simple_https_request);
+        Button btnOkhttp = (Button) findViewById(R.id.okhttp_https_request);
         requestAddress = (EditText) findViewById(R.id.RequestAddress);
 
         btnSimple.setOnClickListener(new btnSimpleClick());
-        btnOKhttp.setOnClickListener(new btnOKhttpClick());
+        btnOkhttp.setOnClickListener(new btnOkhttpClick());
     }
 
     protected String getURL(){
         String newURL = requestAddress.getText().toString().trim();
-        if(newURL.length()==0) {return defaultURL;}
+        if(newURL.length()==0) {
+            return "www.baidu.com";}
         else {return newURL;}
     }
 
     class btnSimpleClick implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    Looper.prepare();
-                    int code = new HttpsRequest().CreateSimpleHttps(getURL());
-                    Toast ero;
-                    if(code!=-1){
-                        ero = Toast.makeText(getApplicationContext(), "Connection Succeed!\nResponse Code:" + code, Toast.LENGTH_SHORT);
-                    }else{
-                        ero = Toast.makeText(getApplicationContext(), "出戳啦\n", Toast.LENGTH_SHORT);
-                    }
-                    ero.show();
-                    Looper.loop();
+            new Thread(() -> {
+                Looper.prepare();
+                int code = new HttpsRequest().CreateSimpleHttps(getURL());
+                Toast ero;
+                if(code!=-1){
+                    ero = Toast.makeText(getApplicationContext(), "Connection Succeed!\nResponse Code:" + code, Toast.LENGTH_SHORT);
+                }else{
+                    ero = Toast.makeText(getApplicationContext(), "出戳啦\n", Toast.LENGTH_SHORT);
                 }
+                ero.show();
+                Looper.loop();
             }).start();
 
         }
     }
 
-    class btnOKhttpClick implements View.OnClickListener {
+    class btnOkhttpClick implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    Looper.prepare();
-                    int code = new HttpsRequest().CreateOkHttps(getURL());
-                    Toast ero;
-                    if(code!=-1){
-                        ero = Toast.makeText(getApplicationContext(), "Connection Succeed!\nResponse Code:" + code, Toast.LENGTH_SHORT);
-                    }else{
-                        ero = Toast.makeText(getApplicationContext(), "出戳啦\n", Toast.LENGTH_SHORT);
-                    }
-                    ero.show();
-                    Looper.loop();
+            new Thread(() -> {
+                Looper.prepare();
+                int code = new HttpsRequest().CreateOkHttps(getURL());
+                Toast ero;
+                if(code!=-1){
+                    ero = Toast.makeText(getApplicationContext(), "Connection Succeed!\nResponse Code:" + code, Toast.LENGTH_SHORT);
+                }else{
+                    ero = Toast.makeText(getApplicationContext(), "出戳啦\n", Toast.LENGTH_SHORT);
                 }
+                ero.show();
+                Looper.loop();
             }).start();
         }
     }
